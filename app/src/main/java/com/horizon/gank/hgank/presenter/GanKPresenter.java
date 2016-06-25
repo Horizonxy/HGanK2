ed3cc976ffe721a1bd9644b8ae008480358d8893
Administrator <864508339@qq.com>
2016-06-25 22:22:17 +0800

    无网络优化

--- app/src/main/java/com/horizon/gank/hgank/presenter/GanKPresenter.java
+++ app/src/main/java/com/horizon/gank/hgank/presenter/GanKPresenter.java
@@ -47,7 +47,6 @@ public class GanKPresenter extends BasePresenter{
                         vGank.onFinish();
                     }
                 }
-                onCompleted();
             }
         });
 
