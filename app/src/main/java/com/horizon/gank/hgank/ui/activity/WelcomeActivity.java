7818567aa4a56342e5896b07030635df19ef479a
Administrator <864508339@qq.com>
2016-07-09 15:25:20 +0800

    初始动画

--- app/src/main/java/com/horizon/gank/hgank/ui/activity/WelcomeActivity.java
+++ app/src/main/java/com/horizon/gank/hgank/ui/activity/WelcomeActivity.java
@@ -2,14 +2,20 @@ package com.horizon.gank.hgank.ui.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import com.horizon.gank.hgank.Application;
 import com.horizon.gank.hgank.MainActivity;
 import com.horizon.gank.hgank.R;
 import com.horizon.gank.hgank.util.SystemStatusManager;
+import com.nostra13.universalimageloader.cache.disc.DiskCache;
+
+import java.io.File;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,6 +31,20 @@ public class WelcomeActivity extends Activity {
         SystemStatusManager.setTranslucentStatusRes(this, R.color.transparent);
         setContentView(R.layout.activity_welcome);
         ButterKnife.bind(this);
+
+        DiskCache diskCache = Application.application.getImageLoader().getDiskCache();
+        File directory = diskCache.getDirectory();
+        if(directory != null && directory.exists() && directory.listFiles() != null && directory.listFiles().length > 0){
+            int index = (int) (Math.random() * (directory.listFiles().length -1));
+            Bitmap bitmap = BitmapFactory.decodeFile(directory.listFiles()[index].getAbsolutePath());
+            if(bitmap == null){
+                vWelcome.setImageResource(R.mipmap.welcome);
+            } else {
+                vWelcome.setImageBitmap(bitmap);
+            }
+        } else {
+            vWelcome.setImageResource(R.mipmap.welcome);
+        }
 
         Animation anim = AnimationUtils.loadAnimation(this, R.anim.splash_start);
         anim.setAnimationListener(new Animation.AnimationListener() {
